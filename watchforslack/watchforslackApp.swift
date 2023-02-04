bdd835db6886e89d2ef2ccd77824fc24a93c8b0e
Cristian Paniagua <tenondecrpc@gmail.com>
2023-02-04 20:18:16 -0300

    Initial Commit

---
+++ watchforslack/watchforslackApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchforslackApp.swift
+//  watchforslack
+//
+//  Created by Cristian Paniagua on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct watchforslackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
